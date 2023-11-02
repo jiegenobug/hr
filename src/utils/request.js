36fe717bcb8a3bc4e34e8a6be1405e7a08a63671
jiege <1768669274@qq.com>
2023-11-02 13:26:22 +0800

    添加请求拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,24 @@
+import store from '@/store'
 import axios from 'axios'
 import { Message } from 'element-ui'
+import { getToken } from './auth'
 const service = axios.create({
     baseURL: process.env.VUE_APP_BASE_API,
     timeout: 5000 // 设置超时时间
 }) // 创建一个axios的实例
-service.interceptors.request.use() // 请求拦截器
+// 请求拦截器
+service.interceptors.request.use(
+    config => {
+        if (store.getters.token) {
+            config.headers['hrsaas-ihrm-token'] = getToken()
+        }
+        return config
+    }, error => {
+        console.log(error);
+        return Promise.reject(error)
+    }
+)
+// 响应拦截器
 service.interceptors.response.use(response => {
     // axios 默认加了一层data
     const { success, message, data } = response.data
@@ -17,5 +31,5 @@ service.interceptors.response.use(response => {
 }, error => {
     Message.error(error.message) // 提示错误信息
     return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入catch
-}) // 响应拦截器
+})
 export default service // 导出axios实例
